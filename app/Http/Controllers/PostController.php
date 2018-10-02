8a8c0cbb7e9b943e6a467111984efb6b26607571
NamDV <35649331+NamDVD00453@users.noreply.github.com>
2018-10-02 14:15:17 +0700

    Add Paginate

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Post;
+use App\TestContent;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('index.home')->with([
+            "listContent" => Post::where('type', 2)->paginate(5)
+        ]);
+    }
+
+    public function loadVideos()
+    {
+        return view('index.videos')->with([
+            "listContent" => Post::where('type', 1)->paginate(5)
+        ]);
+    }
+
+    public function loadSingleVideo($handle_url)
+    {
+        $SingleVideo = Post::where('handle_url', $handle_url)->firstOrFail();
+        return view('index.item')->with(["post" => $SingleVideo]);
+    }
+
+    public function loadSingleImage($handle_url)
+    {
+        $SingleImage = Post::where('handle_url', $handle_url);
+        $SingleImage->increment('seen_count', 1);
+        $SingleImage1 = $SingleImage->firstOrFail();
+        $thisId = $SingleImage1->id;
+
+        $backItem = Post::where('id', $thisId-1)->firstOrFail();
+        $nextItem = Post::where('id', $thisId+1)->firstOrFail();
+
+
+
+        return view('index.image')->with(
+            [
+                "post" => $SingleImage1,
+                "backUrl" => $backItem->handle_url,
+                "nextUrl" => $nextItem->handle_url
+            ]
+        );
+    }
+}
